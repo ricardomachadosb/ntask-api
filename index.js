8bb965d9994c8b19cb549f4d30e656d827792c17
unknown <ricardomachado.sb@gmail.com>
2016-03-15 17:55:35 -0300

    Continuando implementacao

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ const app = express();
 
 app.get("/", (req, res) => res.json({status: "NTask API"}));
 
+app.get("/tasks", (req, res) => {
+	res.json({
+		tasks: [
+			{title: "Fazer compras"},
+			{title: "Consertar o pc"}
+		]
+	});
+});
+
 app.listen(PORT, () => console.log(`NTASK API - porta ${PORT}`));
