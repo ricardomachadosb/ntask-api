a859c1c96aeb6252c5119868b1c4908c78bfb3f3
unknown <ricardomachado.sb@gmail.com>
2016-03-16 11:07:15 -0300

    Capitulo 4

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 import express from "express";
-const PORT = 3000;
+import consign from "consign";
 
 const app = express();
 
-app.get("/", (req, res) => res.json({status: "NTask API"}));
+app.set("json spaces", 4);
 
-app.get("/tasks", (req, res) => {
-	res.json({
-		tasks: [
-			{title: "Fazer compras"},
-			{title: "Consertar o pc"}
-		]
-	});
-});
-
-app.listen(PORT, () => console.log(`NTASK API - porta ${PORT}`));
+consign()
+	.include("models")
+	.then("libs/middlewares.js")
+	.then("routes")
+	.then("libs/boot.js")
+	.into(app);
---
+++ models/tasks.js
@@ -0,0 +1,11 @@
+
+module.exports = app => {
+	return {
+		findAll: (params, callback) => {
+			return callback([
+				{title: "Fazer Compras"},
+				{title: "Consertar pc"}
+			]);
+		}
+	}
+};
