b71eab9e19a8f3ce5126f2fbebfb1cdd4ff0c61d
Ricardo Machado <ricardomachado.sb@gmail.com>
2016-05-03 17:48:06 -0300

    Implementing auth

--- models/users.js
+++ models/users.js
@@ -1,5 +1,9 @@
+import hash from "node_hash";
 
 module.exports = (sequelize, DataType) => {
+
+	const secret = "secret";
+
 	const Users = sequelize.define("Users", {
 		id: {
 			type: DataType.INTEGER,
@@ -21,10 +25,22 @@ module.exports = (sequelize, DataType) => {
 				notEmpty: true
 			}
 		}
-	}, {
+	},{
+		hooks: {
+			beforeCreate: user => {
+				const salt = cfg.hashSalt;
+				var md5 = hash.md5(user.password, salt);
+				user.password = md5;
+			}
+		},
 		classMethods: {
 			associate: (models) => {
 				Users.hasMany(models.Tasks);
+			},
+			isPassword: (encodedPassword, password) => {
+				const salt = secret;
+				var md5 = hash.md5(user.password, salt);
+				return encodedPassword === md5;
 			}
 		}
 	});
